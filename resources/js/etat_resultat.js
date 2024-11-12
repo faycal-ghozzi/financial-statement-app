f5e57b7e4bb67dd60b8207fa7085a967e3dd0723
Faycal Ghozzi <faycal.ghozzi@gmail.com>
2024-11-12 22:04:30 +0100

    fixed resultat exploitation and NaN bug

--- resources/js/etat_resultat.js
+++ resources/js/etat_resultat.js
@@ -4,6 +4,7 @@ $(document).ready(function() {
     calcProduitsExploitation();
     calcChargesExploitation();
     calcCharges();
+    calcDivers();
     formatInputs();
 })
 
@@ -66,22 +67,17 @@ function calcCharges(){
 function calcDivers(){
     $('[data-role^="Activites ordinaires"], [data-role="Charges exploitation"], [data-role="Produits exploitation"]').on('blur', function(){
 
-        // resultats_résultat_dexploitation__n-1
-        
-        let values_n_1 = $('[data-year="n-1"]').map(function() {
-            let value = parseFloat($(this).val().replace(",", ".")) || 0;
-            return $(this).data('role').includes('gains') ? -value : value; 
-        }).get();
+        let total_produits_exploitation_n_1 = parseFloat(cleanNumber($('#resultats_total_produits_dexploitation_n-1').val().replace(",", "."))) || 0
+        let total_charges_exploitation_n_1 = parseFloat(cleanNumber($('#resultats_total_charges_dexploitation_n-1').val().replace(",", "."))) || 0
 
-        let values_n = $('[data-year="n"]').map(function() {
-            let value = parseFloat($(this).val().replace(",", ".")) || 0;
-            return $(this).data('role').includes('gains') ? -value : value; 
-        }).get();
-        
-        let sum_n_1 = values_n_1.reduce((a, b) => a + b, 0);
-        let sum_n = values_n.reduce((a, b) => a + b, 0);
+        let resultats_exploitation_n_1 = total_produits_exploitation_n_1 - total_charges_exploitation_n_1;
+        $('#resultats_résultat_dexploitation__n-1').val(formatNumber(resultats_exploitation_n_1));
 
-        $('#resultats_résultat_des_activités_ordinaires_avant_impôt_n-1').val(formatNumber(sum_n_1));
-        $('#resultats_résultat_des_activités_ordinaires_avant_impôt_n').val(formatNumber(sum_n));  
+        let total_produits_exploitation_n = parseFloat(cleanNumber($('#resultats_total_produits_dexploitation_n').val().replace(",", "."))) || 0
+        let total_charges_exploitation_n = parseFloat(cleanNumber($('#resultats_total_charges_dexploitation_n').val().replace(",", "."))) || 0
+
+        let resultats_exploitation_n = total_produits_exploitation_n - total_charges_exploitation_n;
+
+        $('#resultats_résultat_dexploitation__n').val(formatNumber(resultats_exploitation_n));  
     })
 }
