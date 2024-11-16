c788f3dc18a9662553ec9d40839755c2e11052e1
Faycal Ghozzi <faycal.ghozzi@gmail.com>
2024-11-16 22:53:59 +0100

    added new table

---
+++ database/migrations/2024_11_16_214424_create_financial_statement_files_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('financial_statement_files', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('company_id')->constrained('companies');
+            $table->string('file_path');
+            $table->date('date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('financial_statement_files');
+    }
+};
